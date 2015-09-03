0d301fc9f5a799f3daa659bb3ab16d677d8c7971
myname <gusta.rosn@gmail.com>
2015-09-03 23:25:46 +0200

    added more stuffs

--- lager.c
+++ lager.c
@@ -2,15 +2,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
+
+void welcome();
+int menu();
+void addWare();
+void removeWare();
+int scanInt();
+void clearInput();
+
+struct shelf {
+  char c;
+  int i;
+};
+struct ware
+{
+  char name[20];
+  int price;
+  struct shelf shelf;
+};
+
+int main()
+{
+  welcome();
+  struct ware wares[20];
+  strcpy(wares[0].name, "fs");
+  int svar=0;
+  while (true) {
+    svar = menu();
+    switch (svar) {
+    case 1: addWare(); break;
+    case 8: return 1;
+    default: printf("Critical error.\n"); break;
+    }
+  }
+  return 0;
+}
+
 
 void welcome() {
   printf("Welcome to my storage thingy...\n");
 }
 
 int menu() {
-  int a = 0;
+  int a = 8;
   printf("Select one alternative by entering one integer: ");
-  a = scanInt();
+  //a = scanInt();
   printf("%d\n", a);
   while (a<0 || a>8) {
     printf("Invalid entry.\n");
@@ -21,34 +58,21 @@ int menu() {
 }
 
 void addWare() {
-printf("add ware initiated.\n");
+  printf("add ware initiated.\n");
 }
+
+void removeWare() {
+
+}
+
 
 int scanInt() {
-  int a=0;
-  char s[11];
-  fgets(s, 10, stdin);
-//char c = getc(stdin);
-//a = c - (int)'0';
-  a = atoi(s);
-  return a;
+  clearInput();
+  return 1;
 }
 
-int main()
-{
- 
-  welcome();
-  int svar=0;
-
-//while (true) {
-    svar = menu();
-    switch (svar) {
-    case 1: addWare(); break;
-    case 8: return 1;
-    default: printf("Critical error.\n"); break;
-   }
-// }
-  return 0;
+void clearInput() {
+  //while (getchar!='\n'){}
+  return;
 }
-
 //git config credential.helper "cache"
