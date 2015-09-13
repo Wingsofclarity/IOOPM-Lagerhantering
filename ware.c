695f8999cdf3eca0f71a2a50037c15af5da9363a
myname <gusta.rosn@gmail.com>
2015-09-14 01:07:36 +0200

    setLoc now follows the format

--- ware.c
+++ ware.c
@@ -9,7 +9,7 @@
 
 
 char* getName(Ware *w) {
-  return w->name; //TODO Gives warnging
+  return w->name;
 };
 
 int getPrice(Ware *w) {
@@ -17,7 +17,7 @@ int getPrice(Ware *w) {
 };
 
 char* getLoc(Ware *w) {
-  return w->loc;//TODO Gives warning
+  return w->loc;
 };
 
 void setName(Ware *w, char *s) {
@@ -30,14 +30,36 @@ void setPrice(Ware *w, int a) {
 }
 
 void setLoc(Ware *w, char *s) {
-  strcpy(w->loc, s);
+  char out[4];
+  if (s[0]>='a' && s[0]<='z') {
+    out[0]=(char) s[0]-32;
+  }
+  else if (s[0]<'A' || s[0]>'Z'){
+    out[0]='A';
+    puts("Not a valid input");
+  }
+  else out[0] = s[0];
+  
+  if (s[1]<'0' || s[1]>'9'){
+    out[1]='0';
+    puts("Not a valid input");
+  }
+  else out[1]=s[1];
+  
+  if (s[2]<'0' || s[2]>'9'){
+    out[2]='0';
+    puts("Not a valid input");
+  }
+  else out[2]=s[2];
+  printf("Out is %s \n", out);
+  strcpy(w->loc, out);
 }
 
 void setWare(Ware *w, char *n, int p, char *l) {
   strcpy(w->name, n);
   if (p<0) p=0;
   w->price=p;
-  strcpy(w->loc, l);
+  setLoc(w,l);
 }
 
 //TODO make set functions...
